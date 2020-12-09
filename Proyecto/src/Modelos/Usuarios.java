5be1e16ec76635743e1d4dcb0a9fcc6821c1c622
osval <osval@DESKTOP-4HUEFCT>
2020-12-09 13:56:12 -0600

    Se creo el package Modelos y se anexo el modelo de usuarios

---
+++ Proyecto/src/Modelos/Usuarios.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author osval
+ */
+public class Usuarios {
+    private String NombreUsuario;
+    private String Contraseña;
+    private String Nombre;
+    private String ApellidoP;
+    private String ApellidoM;
+
+    public Usuarios(String NombreUsuario, String Contraseña, String Nombre, String ApellidoP, String ApellidoM) {
+        this.NombreUsuario = NombreUsuario;
+        this.Contraseña = Contraseña;
+        this.Nombre = Nombre;
+        this.ApellidoP = ApellidoP;
+        this.ApellidoM = ApellidoM;
+    }
+
+    public String getNombreUsuario() {
+        return NombreUsuario;
+    }
+
+    public void setNombreUsuario(String NombreUsuario) {
+        this.NombreUsuario = NombreUsuario;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String Contraseña) {
+        this.Contraseña = Contraseña;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellidoP() {
+        return ApellidoP;
+    }
+
+    public void setApellidoP(String ApellidoP) {
+        this.ApellidoP = ApellidoP;
+    }
+
+    public String getApellidoM() {
+        return ApellidoM;
+    }
+
+    public void setApellidoM(String ApellidoM) {
+        this.ApellidoM = ApellidoM;
+    }
+    
+    
+    
+}
